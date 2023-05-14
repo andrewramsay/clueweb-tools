74d1ae562fd0c0eb255aa8cfa6560c8e1dfb0883
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2023-05-14 13:28:38 -0400

    simple script to sort job results by ClueWeb-ID using coreutils 'sort'

---
+++ clueweb_sort_results.py
@@ -0,0 +1,14 @@
+import os
+import argparse
+
+from utils import sort_csv_parallel_sh
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', help='path to folder of unsorted CSV files', required=True, type=str)
+    parser.add_argument('-d', '--destination', help='path to output folder', required=True, type=str)
+    parser.add_argument('-c', '--cores', help='number of cores for "sort" to use', default=8, type=int)
+    parser.add_argument('-b', '--buffersize', help='size of "sort" memory buffer, gigabytes', default=10, type=int)
+    args = parser.parse_args()
+
+    sort_csv_parallel_sh(args.source, args.destination, args.cores, args.buffersize)
